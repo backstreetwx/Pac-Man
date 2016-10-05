7cb22a220cd0293585fe3b9431b05a1d94af33c8
backstreetwx <backstreetwx@gmail.com>
2016-10-05 16:54:09 +0900

    upload the content of the task 4th and 5th

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject Player;
+
+	private Vector3 offset;
+
+
+	void Start () 
+	{
+		offset = transform.position - Player.transform.position;
+	}
+	
+
+	void LateUpdate () 
+	{
+		transform.position = Player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float Speed;
+
+	private Rigidbody rigidbody;
+
+	void Start () 
+	{
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+
+		rigidbody.AddForce (movement * Speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("PickUp"))
+		{
+			other.gameObject.SetActive (false);
+		}
+	}
+}
